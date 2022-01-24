4662bd42e2d03923ee5653c35d46e928e5d72634
DrunkMoonWolf <zemerovanastya2017@gmail.com>
2022-01-24 22:37:19 +0500

    add space

--- MathOper/MathOper/Program.cs
+++ MathOper/MathOper/Program.cs
@@ -6,7 +6,7 @@ namespace MathOper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число a");
+            Console.WriteLine("Введите число a"); 
             double a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите число b");
             double b = Convert.ToDouble(Console.ReadLine());
